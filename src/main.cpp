148a396122eb255bc3552c355097c3bb865fd7bd
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2025-04-03 03:41:03 +0200

    perfect map alignment with odd number of tiles for level

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,23 @@
 #include "ops.h"
 #include "player.h"
 
-const Color BACKGROUND = GetColor(0x282828ff);
 const Color CHECKPOINT = GetColor(0x91eda9ff);
-Vector2 screen{1280, 720};
+
+const Vector2 screen{1280, 720};
+const float tile_size = 40.0f;
+const int cols = screen.x / tile_size;
+const int rows = screen.y / tile_size;
+
 float ball_speed = 300;
 
-enum Screen {
-  Start,
-  Gameplay,
-  Complete,
-  GameOver,
-};
-
-struct Vec2 : public Vector2 {
-  Vec2(float x, float y) : Vector2{x, y} {}
-
-  void norm() {
-    float length = sqrtf(x * x + y * y);
-    if (length > 0) *this /= length;
+void drawGrid() {
+  for (int x = 0; x < cols; x++) {
+    DrawLine(x * tile_size, 0, x * tile_size, screen.y, LIGHTGRAY);
   }
-};
+  for (int y = 0; y < rows; y++) {
+    DrawLine(0, y * tile_size, screen.x, y * tile_size, LIGHTGRAY);
+  }
+}
 
 struct Circle {
   Vector2 pos;
@@ -50,45 +47,50 @@ struct Level {
   Square start;
   Square finish;
   std::vector<Square> checkpoints;
-  std::array<Circle, 5> circles;
+  std::vector<Circle> circles;
   int current_checkpoint = -1;
 
   Level() {
     bounds = {
-      .x = screen.x / 2 - 400,
-      .y = screen.y / 2 - 250,
-      .width = 800,
-      .height = 500,
+      .x = (cols / 2.0f - 8) * tile_size,
+      .y = (rows / 2.0f - 6) * tile_size,
+      .width = 17 * tile_size,
+      .height = 13 * tile_size,
     };
 
     start = {
-      .pos = {bounds.x, bounds.y + bounds.height / 2 - 50},
-      .size = 100,
+      .pos = {bounds.x, bounds.y + bounds.height / 2 - tile_size},
+      .size = 2 * tile_size,
     };
 
     finish = {
-      .pos = {bounds.x + bounds.width - 100, bounds.y + bounds.height / 2 - 50},
-      .size = 100,
+      .pos = {bounds.x + bounds.width - 2 * tile_size, bounds.y + bounds.height / 2 - tile_size},
+      .size = 2 * tile_size,
     };
 
     checkpoints.push_back(Square{
-      .pos = {bounds.x + bounds.width / 2, bounds.y + bounds.height * 0.20f},
-      .size = 50,
+      .pos = {bounds.x + 8 * tile_size, bounds.y + 2 * tile_size},
+      .size = tile_size,
     });
 
     checkpoints.push_back(Square{
-      .pos = {bounds.x + bounds.width / 2, bounds.y + bounds.height * 0.80f - 25 - 50},
-      .size = 50,
+      .pos = {bounds.x + 8 * tile_size, bounds.y + bounds.height - 3 * tile_size},
+      .size = tile_size,
     });
 
     initCircles();
   }
 
   void initCircles() {
-    float length = bounds.width - 200;
-    for (size_t i = 0; i < circles.size(); i++) {
-      circles[i].pos = {bounds.x + 25 + length / 5 * (i + 1), bounds.y + bounds.height / 2};
-      circles[i].radius = 12.5;
+    int num_tiles = abs(start.pos.x + start.size - finish.pos.x) / tile_size;
+
+    for (int i = 0; i < num_tiles; i++) {
+      if (i % 2 == 0) continue;
+      circles.push_back(Circle{
+        .pos = {(bounds.x + (2 + i) * tile_size + tile_size / 2), bounds.y + bounds.height / 2},
+        .radius = 12.5,
+
+      });
     }
   }
 
@@ -108,11 +110,22 @@ struct Level {
 };
 
 struct Game {
+  enum Screen {
+    Start,
+    Gameplay,
+    Complete,
+    GameOver,
+  };
+
   Level level;
   Player player = {.size = {25, 25}, .speed = 250};
   Screen current_screen = Start;
 
-  Game() { InitWindow(screen.x, screen.y, "Tiler"); }
+  Game() {
+    SetTraceLogLevel(LOG_TRACE);
+    TraceLog(LOG_TRACE, "cols=%d rows=%d", cols, rows);
+    InitWindow(screen.x, screen.y, "Tiler");
+  }
 
   void updateStartScreen() {
     level.setPlayer(player);
@@ -127,31 +140,31 @@ struct Game {
 
     player.update(dt);
 
-    for (auto& circle : level.circles) {
-      circle.pos.y += ball_speed * dt;
-
-      if (CheckCollisionCircleRec(circle.pos, circle.radius, player.rect())) {
-        current_screen = GameOver;
-        return;
-      }
-
-      if (circle.pos.y + circle.radius >= level.bounds.y + level.bounds.height ||
-          circle.pos.y - circle.radius <= level.bounds.y) {
-        ball_speed *= -1;
-      }
-    }
-
-    for (size_t i = 0; i < level.checkpoints.size(); i++) {
-      if (CheckCollisionRecs(player.rect(), level.checkpoints[i].rect())) {
-        level.current_checkpoint = i;
-        break;
-      }
-    }
-
-    if (CheckCollisionRecs(player.rect(), level.finish.rect())) {
-      current_screen = Complete;
-      return;
-    }
+    // for (auto& circle : level.circles) {
+    //   circle.pos.y += ball_speed * dt;
+    //
+    //   if (CheckCollisionCircleRec(circle.pos, circle.radius, player.rect())) {
+    //     current_screen = GameOver;
+    //     return;
+    //   }
+    //
+    //   if (circle.pos.y + circle.radius >= level.bounds.y + level.bounds.height ||
+    //       circle.pos.y - circle.radius <= level.bounds.y) {
+    //     ball_speed *= -1;
+    //   }
+    // }
+    //
+    // for (size_t i = 0; i < level.checkpoints.size(); i++) {
+    //   if (CheckCollisionRecs(player.rect(), level.checkpoints[i].rect())) {
+    //     level.current_checkpoint = i;
+    //     break;
+    //   }
+    // }
+    //
+    // if (CheckCollisionRecs(player.rect(), level.finish.rect())) {
+    //   current_screen = Complete;
+    //   return;
+    // }
   }
 
   void updateCompleteScreen() {
@@ -210,7 +223,7 @@ struct Game {
       level.bounds.y,
       level.bounds.width,
       level.bounds.height,
-      WHITE
+      BLACK
     );
   }
 
@@ -242,7 +255,8 @@ struct Game {
 
   void draw() {
     BeginDrawing();
-    ClearBackground(BACKGROUND);
+    ClearBackground(RAYWHITE);
+    drawGrid();
     switch (current_screen) {
       case Start: drawStartScreen(); break;
       case Gameplay: drawGameplayScreen(); break;
